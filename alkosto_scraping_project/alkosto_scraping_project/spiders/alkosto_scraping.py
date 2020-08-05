4716375a749d2f2f5f96b1904ba5e903b4bcc45e
Eduardo Valenzuela Parga <62354692+EduardoVparga@users.noreply.github.com>
2020-08-05 12:28:54 -0500

    Se agrego el script para KTX y se adjunto la seccion de mercado de  ALK

--- alkosto_scraping_project/alkosto_scraping_project/spiders/alkosto_scraping.py
+++ alkosto_scraping_project/alkosto_scraping_project/spiders/alkosto_scraping.py
@@ -1,6 +1,20 @@
 from scrapy import Spider
 from scrapy.http import Request
 
+from selenium import webdriver
+from scrapy.selector import Selector
+from selenium.common.exceptions import ElementClickInterceptedException
+
+from time import sleep
+
+
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
+driver = webdriver.Firefox(options = options)
+
+#driver = webdriver.Firefox()
+
+
 class AlkostoScrapingSpider(Spider):
 	name = 'alkosto_scraping'
 	allowed_domains = ['alkosto.com']
@@ -10,7 +24,7 @@ class AlkostoScrapingSpider(Spider):
 	def parse(self, response):
 
 		try: n_cat = response.meta['n_cat']
-		except: n_cat = 0 ####################################################################################################### OJO!! 
+		except: n_cat = 14 ###################################################################### OJO !!
 
 		print('\n','#'*20, 'Este es n_cat !!', '\n', n_cat)
 
@@ -142,9 +156,9 @@ class AlkostoScrapingSpider(Spider):
 				yield Request(url= 'https://www.alkosto.com/hogar',
 							  callback= self.home_parse,
 							  meta = {'home_n_cat': home_n_cat,
-							  		  'n_cat': n_cat,
-							  		  'categories': categories,
-							  		  'category': category},
+									  'n_cat': n_cat,
+									  'categories': categories,
+									  'category': category},
 							  dont_filter= True)
 			
 			else:
@@ -155,4 +169,108 @@ class AlkostoScrapingSpider(Spider):
 							  meta= {'n_cat': n_cat}) 
 
 		else:
-			print('\n', '='*20,'\n', 'TAL PARECE QUE SE EXTRAJO TODA LA INFORMACION DE LA PAGINA !!','\n', '='*20, '\n')
+			print('\n',response.url)
+			mercado_link = response.xpath('//*[@class= "btn-ver-mas center"]/@href').extract_first()
+			
+			yield Request(url = mercado_link,
+						  callback= self.mercado_parse)
+
+
+
+	def mercado_parse(self, response):
+		print('entro en el parse para mercado')
+		print('\n', response.url)
+
+		try: mercado_n_cat = response.meta['mercado_n_cat']
+		except: mercado_n_cat = 0
+		trys = 1
+
+		while trys <= 5:
+			try:
+				print('\n','*'*10, 'INTENTO # ', trys,'*'*10, '\n')
+				driver.get(response.url)
+				if trys <= 3:
+					sleep(3)
+				else:
+					sleep(5)
+
+				buttons = driver.find_elements_by_xpath('/html/body/div[1]/div/div[2]/div/div/div[2]/div')
+
+				buttons[mercado_n_cat].click()
+				break
+			except:
+				trys +=1
+
+		sleep(3)
+		pag = 1
+		while True:
+			print('\n', '#'*15, 'Estamos en la pagina ', pag,'#'*15, '\n')
+			mercado_pag_sel = Selector(text= driver.page_source)
+			mercado_products = mercado_pag_sel.xpath('.//*[@class= "ProductDisplay-sc-5nadct-0 ldPWHL"]/div')
+
+			cat_name = mercado_pag_sel.xpath('//h1/text()').extract_first()
+			print('\n', '#'*15, 'De la categoria ', cat_name,'#'*15, '\n')
+			print('Vamos en la categoria n ', mercado_n_cat+1, len(buttons))
+			sleep(3)
+
+			try:
+				for mercado_product in mercado_products:
+
+					prod_name = mercado_product.xpath('.//*[@class= "product__content__name"]//text()').extract_first()
+					disc_price = mercado_product.xpath('.//*[@class= "product__content__price__wrapper"]/p/text()').extract()
+					
+					print(disc_price, 'este es disc_price de largo', len(disc_price))
+
+					if len(disc_price) > 1:
+						print('ESTE ENTRA AL IF')
+						normal_price = disc_price[-1]
+						disc_price = disc_price[1]
+					
+					else:
+						print('ESTE ENTRA AL ELSE')
+						normal_price = disc_price[0]
+						disc_price = disc_price[0]
+					
+					image_url = mercado_product.xpath('.//*[@class= "product__figure-wrapper__img"]/@src').extract_first()
+
+					print('\n', '#'*15, 'Resultado Producto', '#'*15, '\n')
+					print('Categoria: ', cat_name, '\n',
+						  '\n\tProducto:\t', prod_name,
+						  '\n\tPrecio norm:\t', normal_price,
+						  '\n\tPrecio desc:\t', disc_price, '\n')
+					
+
+				
+					yield {'cat_name': cat_name,
+						   'prod_name': prod_name,
+						   'normal_price': normal_price,
+						   'disc_price': disc_price,
+						   'image_url': image_url}
+
+				
+				next_button= driver.find_elements_by_xpath('/html/body/div[1]/div/div[2]/div/div/div[2]/ul/li/a')
+				if next_button != []:
+					#next_button= driver.find_elements_by_xpath('/html/body/div[1]/div/div[2]/div/div/div[2]/ul/li/a')[-1].click()
+					next_button[-1].click()
+					sleep(3)
+				else:
+					raise ElementClickInterceptedException()
+				pag += 1
+
+
+			except ElementClickInterceptedException:
+
+				if mercado_n_cat < len(buttons)-1:
+
+					mercado_n_cat += 1
+					yield Request(url = response.url,
+								  callback= self.mercado_parse,
+								  meta= {'mercado_n_cat': mercado_n_cat},
+								  dont_filter= True)
+					break
+					
+				
+				else:
+					print('\n', '='*20,'\n', 'TAL PARECE QUE SE EXTRAJO TODA LA INFORMACION DE LA PAGINA !!','\n', '='*20, '\n')
+					driver.quit()
+					break
